f6f7c981f29b0a42dd8d8d750ae390f61856bdac
su-san1031 <shogo1820061031main@gmail.com>
2026-05-30 18:12:25 +0900

    Add files via upload

---
+++ run_bot.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+echo Discord音楽Botを起動中...
+cd /d "%~dp0"
+
+REM 必要なパッケージがインストールされているかチェック
+echo 依存関係をチェック中...
+py -m pip install -r requirements.txt
+
+echo Botを起動します...
+py src\Hi-FiMusicBot.py
+pause
